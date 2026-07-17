4075f7d93e2dd62aa3a80404ccd3381a0b0ecd98
Isidore-H <3355320948@qq.com>
2026-07-17 18:19:17 +0800

    'feat:首页面板组件'

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     HomeNew
+    <HomePanel></HomePanel>
   </div>
 </template>
 
 <script setup>
-
+import HomePanel from './HomePanel.vue';
 </script>
 
 <style scoped lang="scss">
